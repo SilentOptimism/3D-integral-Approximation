11f28a08cb3a9931c92a2616b76bf9e2bc324367
Joey Dhillon <imjoeydhillon@gmail.com>
2024-11-15 18:45:45 -0800

    math engine working

--- main.cpp
+++ main.cpp
@@ -1,11 +1,140 @@
 #include <iostream>
-#include <iomanip>
-
 using std::cout;
 using std::cin;
 using std::endl;
 
+#include <iomanip>
+
+#include <string>
+using std::string;
+
+#include <sstream>
+
+#include <vector>
+using std::vector;
+
+#include <ctype.h>
+using std::isdigit;
+
 float doubleRectIntegralMidPt(int,int,float[2],float[2]);
+
+float executeFunction(string function){
+
+    int paranthBoth = function.find(")(");
+    int paranthOpen = function.find("(");
+    int paranthClosed = function.find(")");
+
+
+    if(paranthClosed != paranthOpen){
+        string endingOperation = ""; 
+
+        int subFuncSize = paranthClosed-paranthOpen;
+        string subFunc = function.substr(paranthOpen+1,subFuncSize-1);
+
+        if(paranthBoth == paranthClosed){
+            endingOperation = "*";
+        }
+
+        int subFuncValue = executeFunction(subFunc);
+        function.replace(paranthOpen, subFuncSize+1, std::to_string(subFuncValue)+ endingOperation);
+
+        return executeFunction(function);
+
+    }
+    else{
+
+        vector<char> operations;
+        vector<int> numbers;
+
+        // Gets our operations '+', '-', '/', etc
+        for(char& c: function){
+            if(!isdigit(c)){
+                operations.push_back(c);
+                c = ' ';
+            }
+        }
+
+        // Gets our numbers 234, 12, 536
+        int temp = 0;
+        std::stringstream ss(function);
+        while(ss){
+            ss >> temp;
+            if(ss){numbers.push_back(temp);}
+        }
+
+        // Multiplication and Division
+        vector<char>::iterator opStart = operations.begin();
+        vector<char>::iterator opEnd = operations.end();
+        int index = 0;
+        while(opStart!=opEnd){
+            if(*(opStart) == '*'){
+                int num1 = numbers.at(index);
+                int num2 = numbers.at(index+1);
+
+                operations.erase(opStart);
+                numbers.erase(numbers.begin() + index+1);
+
+                numbers.at(index) = num1*num2;
+                index--;
+
+            }
+            else if(*(opStart) == '/'){
+                int num1 = numbers.at(index);
+                int num2 = numbers.at(index+1);
+
+                operations.erase(opStart);
+                numbers.erase(numbers.begin() + index+1);
+
+                numbers.at(index) = num1/num2;
+                index--;
+            }
+
+            opStart++;
+            index++;
+        }
+
+        // Addition/Subtraction
+        opStart = operations.begin();
+        opEnd = operations.end();
+        index = 0;
+        while(opStart!=opEnd){
+            if(*(opStart) == '+'){
+                int num1 = numbers.at(index);
+                int num2 = numbers.at(index+1);
+
+                operations.erase(opStart);
+                numbers.erase(numbers.begin() + index+1);
+
+                numbers.at(index) = num1+num2;
+                index--;
+            }
+            else if(*(opStart) == '-'){
+                int num1 = numbers.at(index);
+                int num2 = numbers.at(index+1);
+
+                operations.erase(opStart);
+                numbers.erase(numbers.begin() + index+1);
+
+                numbers.at(index) = num1-num2;
+                index--;
+            }
+
+            opStart++;
+            index++;
+        }
+
+        return numbers.at(0);
+    }
+}
+    
+string insertMath(string function, float x, float y){
+    int xindex = 0;
+    int yindex = 0;
+
+    while(xindex = function.find('x') <= 0){
+    }
+
+}
 
 int main(int argc, char const *argv[])
 {
@@ -15,10 +144,14 @@ int main(int argc, char const *argv[])
     float xRange[2] = {1,2};
     float yRange[2] = {1,2};
 
-
     doubleRectIntegralMidPt(4,2,xRange, yRange);
 
-    
+   string func = "(5+6)(35*8+2)(1+8)";
+
+   cout << executeFunction(func);
+
+
+
 
     return 0;
 }
@@ -51,6 +184,3 @@ float doubleRectIntegralMidPt(int m, int n, float xRange[1], float yRange[1]){
     return 0;
 }
 
-float function(){
-
-}
